99ef9efbdcc4a63bf4041f3fa7f5d4c6ff48eddc
수연 임 <s2129@e-mirim.hs.kr>
2022-03-22 10:59:44 +0900

    증가연산자

---
+++ 201/C기본/17증가 연산자.c
@@ -0,0 +1,15 @@
+﻿#include <stdio.h>
+
+int main(void)
+{
+	int a, b, c = 0;
+	a = ++c; //c먼저 1증가  a=1 b=0 c=1
+	b = c++; //b에 대입하고 c 증가 a=1 b=1 c=2
+	printf("%d %d %d\n", a, b, ++c); //a=1 b=1 c=3
+
+	return 0;
+}
+
+
+
+
