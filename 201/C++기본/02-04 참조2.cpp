f1affb69a3ddee3592f6c64d601b3dfc45bf9050
수연 임 <s2129@e-mirim.hs.kr>
2022-04-05 11:37:30 +0900

    Call by 레퍼런스(참조)

--- 201/C++기본/02-04 참조2.cpp
+++ 201/C++기본/02-04 참조2.cpp
@@ -2,17 +2,17 @@
 using namespace std;
 
 
-void plus2(int* num)
+void plus2(int& num)
 {
-	//num이 포인터 변수이기에 *로 써서 내부값을 처리
-	*num += 2;
+	//num이 참조형 변수이기에 *를 안써서 내부값을 처리
+	num += 2;
 }
 
 int main(void) {
 
 	int a = 3;
-	//포인터 매개변수 num에게 주소값으로 전달
-	plus2(&a);
+	//참조형 매개변수 num에게 주소값으로 전달
+	plus2(a);
 	cout << a << endl;
 
 	return 0;
