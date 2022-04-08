56d67b55f7464795bfc39318bfacf615e8b32420
수연 임 <s2129@e-mirim.hs.kr>
2022-04-08 09:54:09 +0900

    string 시작주소

---
+++ 201/C++기본/02-10 string 시작주소.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>//cout, endl 사용
+#include<stdio.h> //printf사용
+
+#include<string>//std::string클래스 사용
+
+using namespace std;
+
+
+int main(void)
+{
+	char cI[] = "aaa";
+	printf("%s \n", cI); //aaa
+
+	string cppl = "aaa";
+	printf("%s \n", cppl.c_str()); //aaa
+	cout << cppl.c_str() << endl;
+}
