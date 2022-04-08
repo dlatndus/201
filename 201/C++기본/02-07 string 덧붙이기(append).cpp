70bfdd69b63945571c12dd9cc733d7964e22410a
수연 임 <s2129@e-mirim.hs.kr>
2022-04-08 09:04:02 +0900

    string 덧붙이기(append)

--- 201/C++기본/02-07 string 덧붙이기(append).cpp
+++ 201/C++기본/02-07 string 덧붙이기(append).cpp
@@ -8,11 +8,14 @@ using namespace std;
 int main(void) {
 
 	char cMunja[20];
-	strcpy(cMunja, "c_insert");
+	strcpy(cMunja, "c_insert");	//c_insert
+	strcat(cMunja, "++");		//c_insert++
 	printf("%s\n", cMunja);
 
-	/*std::*/string cppMunja; //using 써서 안써도 오류 안남
-	cppMunja = "cpp_insert";
+	//주의사항 "cpp_insert" + "++"; 하면 에러발생
+	string cppMunja; //using 써서 안써도 오류 안남
+	cppMunja = "cpp_insert";	//cpp_insert
+	cppMunja = cppMunja + "++";	//cpp_insert++
 	cout << cppMunja << endl;
 
 	return 0;
