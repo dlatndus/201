c95cc9fa7445b4991cc1cd1ba5233c13a1362b5c
수연 임 <s2129@e-mirim.hs.kr>
2022-04-08 09:43:53 +0900

    string 길이

---
+++ 201/C++기본/02-09 string 길이.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>//cout, endl 사용
+#include<stdio.h> //printf사용
+#include<string.h>//strlen사용
+#include<string>//std::string클래스 사용
+
+using namespace std;
+
+
+int main(void)
+{
+	char cI[] = "aaa";
+	printf("%d \n", strlen(cI)); //3
+
+	string cppl = "aaa";
+	cout << cppl.size() << endl;	//3
+	cout << cppl.length() << endl;	//size()와 같은 결과
+}
