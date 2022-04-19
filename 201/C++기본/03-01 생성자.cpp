cbceafa9b07c68497186924e48ae351b74dc5ee0
수연 임 <s2129@e-mirim.hs.kr>
2022-04-19 12:13:20 +0900

    생성자 오버로딩

--- 201/C++기본/03-01 생성자.cpp
+++ 201/C++기본/03-01 생성자.cpp
@@ -1,38 +1,44 @@
-﻿#include <iostream>//cout, endl 사용
-#include<string>
+﻿#include <iostream>
+#include <string>
 
 using namespace std;
 
-class Student
-{
-private :
+class student {
+private:
 	int nHakbun;
 	string sName;
 
+public:
 	//생성자 : 객체가 생성되면 자동으로 호출되는 함수
-	//반환형을 갖고있지 않는다
-public :
-	Student();
+	//반환형을 갖고있지 않는다.
+	student();
+	student(int Hakbun, string Name);
+
 	void show();
 
-
 };
-
-Student :: Student()
-{
+student::student() {
 	nHakbun = 1234;
 	sName = "이사랑";
-	cout << "학번이 등록되었습니다" << endl;
+	cout << "학번이 등록되었습니다." << endl;
+
+}
+student::student(int Hakbun, string Name)
+{
+	nHakbun = Hakbun;
+	sName = Name;
+	cout << "학번이 등록되었습니다." << endl;
 }
 
-void Student :: show()
-{
+void student::show() {
 	cout << "학번은" << nHakbun << "입니다" << endl;
-	cout << "이름은" << sName << "입니다" << endl;
+	cout << "이름은" << sName << "입니다" << endl << endl;
+}
+int main(void) {
+	student stu1 = student();
+	stu1.show();
+
+	student stu2 = student(1111, "JWP");
+	stu2.show();
 }
 
-int main(void)
-{
-	Student stu;
-	stu.show();
-}
