fc4fb2213a220fefda332a4f6b686f1f34e10743
MiJi <MiJi@School>
2022-06-03 10:13:51 +0900

    연산자 오버로딩2

--- 201/C++기본/04-01 연산자 오버로딩(기본).cpp
+++ 201/C++기본/04-01 연산자 오버로딩(기본).cpp
@@ -38,9 +38,9 @@ int main(void)
 {
 	Point p1(1, 2);
 	Point p2(3, 4);
-	Point sum = p1.operator+(p2);	// p1 + p2
+	Point sum = p1 + p2;	//  p1.operator+(p2)
 	
-	sum.print();	// (4, 6) 출력
+	sum.print();			// (4, 6) 출력
 	
 	return 0;
 }
