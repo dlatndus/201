fa4031db2f3e855e2b5076b250888d9c6c0b1e4f
수연 임 <s2129@e-mirim.hs.kr>
2022-06-03 09:47:59 +0900

    연산자 오버로딩 밑밥깔기

---
+++ 201/C++기본/04-01 연산자 오버로딩(기본).cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+using namespace std;
+
+class Point {
+private:
+	int mX;
+	int mY;
+public:
+	Point(int _x, int _y);
+	void print(void);
+
+
+};
+
+Point::Point(int _x, int _y)
+	: mX(_x), mY(_y)
+{
+	
+}
+
+void Point::print(void) 
+{
+	cout << "mX : "<< mX <<" mY : "<< mY << endl;
+}
+
+int main(void)
+{
+	Point p1(1, 2);
+	p1.print();
+
+}
---
+++ 201/C++기본/교과서 130쪽 2번문제.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include<string>
+using namespace std;
+
+class CBank {
+private:
+	string name;
+	string account;
+	int balance;
+public:
+	CBank(string name, string account, int balance);
+	void deposit(int amount);
+};
+
+CBank::CBank(string name, string account, int balance) 
+	:name(name), account(account), balance(balance)
+{
+	cout << "예금주 : " << name << " 계좌번호 : " << account << " 잔액 : " << balance << endl;
+}
+
+void CBank::deposit(int amount) {
+	cout << "입금된 금액은" << amount << "원 입니다" << endl;
+	balance += amount;
+}
+
+int main(void) {
+	CBank c1 = CBank("as", "as", 1000);
+	c1.deposit(1000);
+
+	return 0;
+}
