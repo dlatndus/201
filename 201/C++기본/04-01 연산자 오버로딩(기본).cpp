f22c0e23a54564de476f9b7ca1af37ce7ae41f50
수연 임 <s2129@e-mirim.hs.kr>
2022-06-03 10:22:35 +0900

    파일 분할

--- 201/C++기본/04-01 연산자 오버로딩(기본).cpp
+++ 201/C++기본/04-01 연산자 오버로딩(기본).cpp
@@ -1,45 +1,6 @@
 ﻿#include <iostream>
+#include "04-01.h"
 using namespace std;
-
-class Point {
-public:
-	//생성자
-	Point(int _x, int _y);
-	Point();
-
-	//연산자 오버로딩
-	Point operator+(const Point& rhs);
-
-	void print(void);
-
-private:
-	int mX;
-	int mY;
-
-
-
-};
-
-Point::Point(int _x, int _y)
-	: mX(_x), mY(_y)
-{
-	
-}
-
-Point::Point(){}
-
-void Point::print(void) 
-{
-	cout << "mX : "<< mX <<" mY : "<< mY << endl;
-}
-
-Point Point::operator+(const Point& rhs)//=>오른쪽에 있는 객체 말함
-{
-	Point p;//매개변수 없는 생성자가 필요
-	p.mX = mX + rhs.mX;
-	p.mY = mY + rhs.mY;
-	return p;
-}
 
 int main(void)
 {
---
+++ 201/C++기본/04-01.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+#include "04-01.h"
+using namespace std;
+
+
+Point::Point(int _x, int _y)
+	: mX(_x), mY(_y)
+{
+	
+}
+
+Point::Point(){}
+
+void Point::print(void) 
+{
+	cout << "mX : "<< mX <<" mY : "<< mY << endl;
+}
+
+Point Point::operator+(const Point& rhs)//=>오른쪽에 있는 객체 말함
+{
+	Point p;//매개변수 없는 생성자가 필요
+	p.mX = mX + rhs.mX;
+	p.mY = mY + rhs.mY;
+	return p;
+}
+
