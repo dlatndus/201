215d61768ca900de31bc00420af066a32168a1c4
MiJi <MiJi@School>
2022-06-07 12:10:41 +0900

    대입연산자(default)

--- 201/C++기본/04-03 대입연산자 오버로딩.cpp
+++ 201/C++기본/04-03 대입연산자 오버로딩.cpp
@@ -17,6 +17,8 @@ public:
 	Student(const Student& rhs);
 	~Student();
 
+	Student& operator=(const Student& rhs);
+
 	void show();
 
 };
@@ -58,15 +60,29 @@ void Student::show()
 	cout << "이름은 " << sName << "입니다" << endl << endl;
 }
 
+// default 대입연산자
+Student& Student::operator=(const Student& rhs)
+{
+	nHakbun = rhs.nHakbun;
+	sName = rhs.sName;
+	
+	return *this;
+}
+
 int main(void)
 {
 	// "일반생성자 호출" 출력
 	Student stu1 = Student(1111, "JWP");
-	// (1111, "JWP")가 복사됨. 일반생성자 호출X
+	Student stu3 = Student(2222, "JYP");
+	stu1.show();		// (1111, "JWP")
+
+	// 복사생성자 호출
 	Student stu2 = stu1;	// stu2 = Student(stu1)
-	
-	stu1.show();
-	stu2.show();
+	stu2.show();		// (1111, "JWP")
+
+	// 대입연산자 호출 (아직 오버로딩 구현 안함)
+	stu1 = stu3;
+	stu1.show();		// (2222, "JYP")
 
 	return 0;
 }
