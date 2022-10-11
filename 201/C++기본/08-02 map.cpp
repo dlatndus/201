936db5923f1abe206cb56faf5d58b8a330259a7c
임수연 <81746616+dlatndus@users.noreply.github.com>
2022-10-11 11:44:31 +0900

    map을 사용하는 또 다른 방법

--- 201/C++기본/08-02 map.cpp
+++ 201/C++기본/08-02 map.cpp
@@ -14,22 +14,18 @@ using namespace std;
 void main(void)
 {
 	map<string, string> ONEUS; //ONEUS
-	ONEUS.insert(pair<string, string>("김영조", "레이븐"));
-	ONEUS.insert(pair<string, string>("이서호", "서호"));
-	ONEUS.insert(pair<string, string>("김건학", "이도"));
-	ONEUS.insert(pair<string, string>("이건희", "건희"));
-	ONEUS.insert(pair<string, string>("여환웅", "환웅"));
-	ONEUS.insert(pair<string, string>("손동주", "시온"));
+	ONEUS["김영조"] = "레이븐";
+	ONEUS["이서호"] = "서호";
+	ONEUS["김건학"] = "이도";
+	ONEUS["이건희"] = "건희";
+	ONEUS["여환웅"] = "환웅";
+	ONEUS["손동주"] = "시온";
 
 	map<string, string>::iterator iter;
 	for (iter = ONEUS.begin(); iter != ONEUS.end(); iter++)
 		cout << iter->first << "은 " << iter->second << "이다" << endl; //key  v
 
-	//여환웅이란 key를 가지는 iterator
-	map<string, string>::iterator first_love = ONEUS.find("여환웅");
 
-	//해당 key 가 존재하는지 체크
-	if(first_love != ONEUS.end())
-		cout << "저의 최애는 " << first_love->second << endl;
+		cout << "저의 최애는 " << ONEUS["여환웅"] << endl;
 
 }
